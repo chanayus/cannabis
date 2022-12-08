91bce810ec5a558c09ab9b8a6e71be14559a78ee
chanayus <chanayus1526@hotmail.com>
2022-12-08 16:38:13 +0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -2,48 +2,33 @@ import dynamic from "next/dynamic"
 import { motion } from "framer-motion"
 import Navbar from "../components/Navbar"
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 
 const Footer = dynamic(() => import("./Footer"), {
   ssr: false,
 })
 
 const Layout = ({ children }) => {
-  const [firstRender, setFirstRender] = useState(true)
   const router = useRouter()
 
   useEffect(() => {
     window.history.scrollRestoration = "manual"
-    if (firstRender) {
-      window.scroll(0, 0)
-      setFirstRender(false)
-    } else {
-      setTimeout(() => {
-        window.scroll(0, 0)
-      }, 350)
-    }
-    return () => {
-      clearTimeout(
-        setTimeout(() => {
-          window.scroll(0, 0)
-        }, 350)
-      )
-    }
+    window.scroll(0, 0)
   }, [router.pathname])
 
   return (
     <>
       <Navbar />
-        <motion.div
-          exit={{ opacity: 0 }}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.3 }}
-          key={router.pathname}
-          className="w-full min-h-[calc(100vh-200px)] py-[7.5vh] md:pt-[74px] pt-[65px]"
-        >
-          {children}
-        </motion.div>
+      <motion.div
+        exit={{ opacity: 0 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.35 }}
+        key={router.pathname}
+        className="w-full min-h-[calc(100vh-200px)] py-[7.5vh] md:pt-[74px] pt-[65px]"
+      >
+        {children}
+      </motion.div>
       <Footer />
     </>
   )
